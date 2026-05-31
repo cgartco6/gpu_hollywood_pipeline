b38a8c86d81abced12af52a56e40b6cbe5d38850
cgartco6 <155203611+cgartco6@users.noreply.github.com>
2026-05-31 17:17:07 +0200

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,11 @@
+from .video_diffusion import VideoDiffusionModel
+from .lipsync import LipSyncEngine
+from .tts import TTSModel
+from .music import MusicModel
+
+__all__ = [
+    "VideoDiffusionModel",
+    "LipSyncEngine",
+    "TTSModel",
+    "MusicModel"
+]
